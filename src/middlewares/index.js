2db67f277d0f47e189dd074780f404beb692bc1e
Farhan Sayyed <sayyedfarhan8286@gmail.com>
2026-04-17 18:00:09 +0530

    feat: remove unused middleware and utility exports

--- src/middlewares/index.js
+++
@@ -1,9 +0,0 @@
-const { authGuard } = require("../common/middleware/auth.middleware");
-const { roleGuard } = require("../common/middleware/role.middleware");
-const { validateRequest } = require("./validateRequest");
-
-module.exports = {
-  authGuard,
-  roleGuard,
-  validateRequest
-};
--- src/utils/index.js
+++
@@ -1,7 +0,0 @@
-const { apiResponse } = require("../common/utils/apiResponse");
-const { asyncHandler } = require("../common/utils/asyncHandler");
-
-module.exports = {
-  apiResponse,
-  asyncHandler
-};
